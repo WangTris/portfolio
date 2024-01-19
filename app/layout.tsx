3a3153648d75bbcc991027ab18d5e714cd0ede97
TriDev <tranwangtristudy@gmail.com>
2024-01-19 22:24:02 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import Header from "@/components/header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Tri | Portfolio",
+  description: "Tran Quang Tri's personal portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="absolute left-[35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
